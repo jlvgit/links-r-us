236251250c5c2828696e701f51c79dd65f157fc7
jlvgit <jlv44@ymail.com>
2016-02-14 20:14:44 -0500

    fix update issue

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,7 @@
 class LinksController < ApplicationController
-  before_action :authorized_user, only: [:edit, :update, :destroy]
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authorized_user, only: [:edit, :update, :destroy]
 
   # GET /links
   # GET /links.json
@@ -58,7 +58,7 @@ class LinksController < ApplicationController
   def destroy
     @link.destroy
     respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully deleted.' }
+      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -68,25 +68,26 @@ class LinksController < ApplicationController
     @link.upvote_by current_user
     redirect_to :back
   end
-   
+
   def downvote
     @link = Link.find(params[:id])
-    @link.downvote_by current_user
+    @link.downvote_from current_user
     redirect_to :back
   end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
       @link = Link.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def link_params
-      params.require(:link).permit(:title, :url)
-    end
-
     def authorized_user
       @link = current_user.links.find_by(id: params[:id])
       redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def link_params
+      params.require(:link).permit(:title, :url)
     end
 end
